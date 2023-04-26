d058058cdd725fd5d771363b6f324913c95db7a5
Mark Smith <totallynotmark6@gmail.com>
2023-04-25 22:09:26 -0500

    question 14

---
+++ practice_problems/Question14.java
@@ -0,0 +1,23 @@
+package practice_problems;
+
+public class Question14 {
+    /**
+     * Returns the largest number in an array.
+     * @param lst the array to search for the largest number
+     * @return the largest number in lst
+     */
+    public static int largestNum(int[] lst) {
+        int largest = lst[0];
+        for (int i = 1; i < lst.length; i++) {
+            if (lst[i] > largest) {
+                largest = lst[i];
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        int[] lst = {1, 2, 3, 4, 5};
+        System.out.println("largestNum({1, 2, 3, 4, 5}) -> " + largestNum(lst));
+    }
+}
